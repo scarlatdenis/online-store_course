5ef91e504325acfd7884900db4760a97db1c681a
denis <denis.scarlat@gmail.com>
2021-05-03 12:02:58 +0300

    28.33  added routes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 const sequelize = require("./db");
 const express = require("express");
 const models = require("./models/models");
+const cors = require("cors"); // for req res from browser
+const router = require("./routes/index");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json()); //express is for our app can be parse json format
+app.use("/api", router);
+
 const start = async () => {
   try {
     await sequelize.authenticate();
