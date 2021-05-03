eae819172c51bdb1d9ec160081e149aca2c85a04
denis <denis.scarlat@gmail.com>
2021-05-03 10:55:13 +0300

    23.39 created models and connected to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const sequelize = require("./db");
 const express = require("express");
+const models = require("./models/models");
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,7 +9,7 @@ const app = express();
 const start = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync(); 
+    await sequelize.sync();
     app.listen(PORT, () =>
       console.log(`Server has been started on port ${PORT} `)
     );
